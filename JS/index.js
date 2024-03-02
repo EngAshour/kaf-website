3b4bebe9823d8b105ea5f9df00af4856b14af3ff
Ashour <eng.mohammadashour@gmail.com>
2024-03-02 22:55:01 +0200

    feat: Implement profile menu logic

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,9 @@
+// Declaration
+const profile = document.getElementById("profilePic");
+const menu = document.getElementById("accountMenu");
 
+// Account Menu Logic
+
+profile.addEventListener("click", () => {
+  menu.classList.toggle("hide");
+});
