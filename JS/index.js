8d2bf09111ff241ffe5b16e2545ed2c6a0a083ee
Ashour <eng.mohammadashour@gmail.com>
2024-03-02 23:01:20 +0200

    feat: Implement img slider logic and UI

--- JS/index.js
+++ JS/index.js
@@ -2,6 +2,21 @@
 const profile = document.getElementById("profilePic");
 const menu = document.getElementById("accountMenu");
 
+const arrOfImgs = document.getElementsByClassName("carousel-item");
+
+const sliderNextBtn = document.getElementsByClassName(
+  "carousel-control-next"
+)[0];
+const sliderPrevBtn = document.getElementsByClassName(
+  "carousel-control-prev"
+)[0];
+
+const nextButtonSpan = sliderNextBtn.querySelector("span");
+const prevButtonSpan = sliderPrevBtn.querySelector("span");
+
+const sliderDotsBox = document.getElementsByClassName("carousel-indicators")[0];
+const silderDots = sliderDotsBox.querySelectorAll("button");
+
 // Account Menu Logic
 
 profile.addEventListener("click", () => {
